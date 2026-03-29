ffcbaee04630677ba1842ada95cec70b5f40c19e
Notebook2 <wjswlsdh159@gmail.com>
2026-03-29 14:48:29 +0900

    과제 끄읕

---
+++ assignment_week3/assignment/Sample/Domain/IDamageable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment.Sample.Domain
+{
+    public interface IDamageable
+    {
+        public void SetHP(int val);
+    }
+}
---
+++ assignment_week3/assignment/Sample/Infrastructure/Player.cs
@@ -0,0 +1,45 @@
+﻿using assignment.Sample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment.Sample.Infrastructure
+{
+    public class Player : IPlayer, IGrowable, IMovableObject, IDamageable
+    {
+        public string ID { get; }
+        private TransformData _tr;
+        private StatusValue _status;
+
+        public Player() { }
+        public Player(string id)
+        {
+            this.ID = id;
+            _tr = new TransformData(0f, 0f); // 초기 생성자
+            _status = new StatusValue(100, 0); // 초기 생성자
+        }
+        /// <summary>
+        /// IGrowable 인터페이스 구현
+        /// </summary>
+        public void SetEXP (int val)
+        {
+            _status.SetEXP(val);
+        }
+        /// <summary>
+        /// IMovableObject 인터페이스 구현
+        /// </summary>
+        public void Move(float x, float y)
+        {
+            _tr.SetX(x);
+            _tr.SetY(y);
+            Console.WriteLine($"[Player {ID}] 이동 완료 : ({_tr.X}, {_tr.Y})");
+        }
+        /// <summary>
+        /// IDamageable 인터페이스 구현
+        /// </summary>
+        public void SetHP(int val)
+        {
+            _status.SetHP(val);
+        }
+    }
+}
